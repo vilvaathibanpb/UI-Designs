7f68f5337a9791d6b4c158aab449a754c516178c
rahul <rahulupadhyay13@gail.com>
2017-12-04 11:12:58 +0530

    add the agreement Header annimation

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM, { render } from 'react-dom';
-import Home from './components/Home/Home';
-import AboutUs from './components/AboutUs/AboutUs';
-import Enquiry from './components/Enquiry/Enquiry';
-import Documents from './components/Documents/Documents';
-import AgreementProduct from './components/AgreementProducts/AgreementProduct';
-import RegisterMain from './components/Register/RegisterMain';
-// import SearchBox from '/home/rahul/Desktop/dockettech_frontend/src/components/Home/SearchBox/SearchBox.jsx';
-
-
-
-render(<AgreementProduct />, document.getElementById("app"));
-=======
 import { render } from 'react-dom';
 import App from './App/App';
 import { Provider } from 'react-redux';
@@ -22,4 +8,3 @@ import { store } from './helpers/store';
 
 
 render(<Provider store={store}><App /></Provider>, document.getElementById("app"));
->>>>>>> 45f23a6b07bdd4c35a18e54b68b271ae39125dc5
