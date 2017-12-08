bdad70caa85bcfd3e72b02642c4df506da9c0687
rahul <rahulupadhyay13@gail.com>
2017-12-08 11:36:08 +0530

    revert the dropdown in home page

--- src/components/Home/SearchBox/SearchBox.jsx
+++ src/components/Home/SearchBox/SearchBox.jsx
@@ -8,32 +8,32 @@ export default class SearchBox extends Component {
     constructor(props) {
         super(props);
     }
-    // toggleMenu() {
-    //     var menuBox = document.getElementById('menu-box');  
-    //     if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
-    //       menuBox.style.display = "none";
-    //     }
-    //     else { // if is menuBox hidden, display it
-    //       menuBox.style.display = "block";
-    //     }
-    //   }
+    toggleMenu() {
+        var menuBox = document.getElementById('menu-box');  
+        if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+          menuBox.style.display = "none";
+        }
+        else { // if is menuBox hidden, display it
+          menuBox.style.display = "block";
+        }
+      }
     /* When the user clicks on the button, 
         toggle between hiding and showing the dropdown content */
-         myFlexitem(id) {
-            if (event.target.matches('.dropbtn')) {
+        //  myFlexitem(id) {
+        //     if (event.target.matches('.dropbtn')) {
 
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-              var openDropdown = dropdowns[i];
-              if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-              }
-            }
-          }
-            // document.getElementsByClassName('dropdown-content').classList.remove("show");
-            document.getElementById(id).classList.toggle("show");
-        }
+        //     var dropdowns = document.getElementsByClassName("dropdown-content");
+        //     var i;
+        //     for (i = 0; i < dropdowns.length; i++) {
+        //       var openDropdown = dropdowns[i];
+        //       if (openDropdown.classList.contains('show')) {
+        //         openDropdown.classList.remove('show');
+        //       }
+        //     }
+        //   }
+        //     // document.getElementsByClassName('dropdown-content').classList.remove("show");
+        //     document.getElementById(id).classList.toggle("show");
+        // }
 
         // Close the dropdown if the user clicks outside of it
         // window.onclick = function(event) {
@@ -62,15 +62,15 @@ export default class SearchBox extends Component {
                     </div>
                 </div>
                 <div className="flex-container">
-                    <div className="flex-item" onClick={this.myFlexitem.bind('document')}>
+                    <div className="flex-item" onClick={this.toggleMenu.bind('document')}>
                         <span className="dropbtn">DOCUMENTS <i className="fa fa-caret-down" /></span>
-                        <div id="document" className="dropdown-content">
+                        <div id="menu-box" className="dropdown-content">
                             <a href="#">Documents1</a>
                             <a href="#">Documents2</a>
                             <a href="#">Documents3</a>
                         </div>
                     </div>
-                    <div className="flex-item dropbtn verticalLine" onClick={this.myFlexitem.bind('list-box')}>
+                    <div className="flex-item dropbtn verticalLine" onClick={this.toggleMenu.bind('list-box')}>
                         ON DEMAND <i className="fa fa-caret-down" />
                         <div id="list-box" className="dropdown-content">
                             <a href="#">List1</a>
@@ -78,7 +78,7 @@ export default class SearchBox extends Component {
                             <a href="#">List3</a>
                         </div>
                     </div>
-                    <div className="flex-item dropbtn verticalLine" onClick={this.myFlexitem.bind('startAbusiness')}>
+                    <div className="flex-item dropbtn verticalLine" onClick={this.toggleMenu.bind('startAbusiness')}>
                         START A BUSINESS  <i className="fa fa-caret-down" />
                         <div id="startAbusiness" className="dropdown-content">
                             <a href="#">Item1</a>
@@ -86,7 +86,7 @@ export default class SearchBox extends Component {
                             <a href="#">Item3</a>
                         </div>
                     </div>
-                    <div className="flex-item dropbtn verticalLine" onClick={this.myFlexitem.bind('compliances')}>
+                    <div className="flex-item dropbtn verticalLine" onClick={this.toggleMenu.bind('compliances')}>
                         COMPLIANCES <i className="fa fa-caret-down" />
                         <div id="compliances" className="dropdown-content">
                             <a href="#">Item1</a>
@@ -94,7 +94,7 @@ export default class SearchBox extends Component {
                             <a href="#">Item3</a>
                         </div>
                     </div>
-                    <div className="flex-item dropbtn verticalLine" onClick={this.myFlexitem.bind('property')}>
+                    <div className="flex-item dropbtn verticalLine" onClick={this.toggleMenu.bind('property')}>
                         PROPERTY <i className="fa fa-caret-down" />
                         <div id="property" className="dropdown-content">
                             <a href="#">PROPERTY1</a>
@@ -102,7 +102,7 @@ export default class SearchBox extends Component {
                             <a href="#">PROPERTY3</a>
                         </div>
                     </div>
-                    <div className="flex-item dropbtn verticalLine" onClick={this.myFlexitem.bind('licences')}>
+                    <div className="flex-item dropbtn verticalLine" onClick={this.toggleMenu.bind('licences')}>
                         LICENCES <i className="fa fa-caret-down" />
                         <div id="licences" className="dropdown-content">
                             <a href="#">LICENCES1</a>
